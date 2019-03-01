9e70c4f61786a49adc07f2cfbf119d91b4280f7e
hannahdonaldson <hannahcdonaldson@gmail.com>
2019-03-01 14:32:58 -0700

    Add a lot of new files

---
+++ src/components/pages/blog.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function() {
+    return(
+        <div>
+            <h2>Blog</h2>
+
+            <Link to = '/about-me'>Read more about me!</Link>
+        </div>
+    );
+}
--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,57 +1,70 @@
 import React, { Component } from "react";
-
-import PortfolioItem from './portfolio-item';
-
+import axios from "axios";
+import PortfolioItem from "./portfolio-item";
 export default class PortfolioContainer extends Component { // extends Component (this INHERITS)
-    constructor() {
-        super();
+  constructor() {
+    super();
+    this.state = {
+      pageTitle: "Welcome to my portfolio",
+      isLoading: false,
+      data: []
+    };
+    this.handleFilter = this.handleFilter.bind(this);
+  }
 
-        this.state = {
-            pageTitle: "Welcome to my portfolio",
-            isLoading: false,
-            data: [
-                {title: "Quip", category: "eCommerce"},
-                {title: "Eventbrite", category: "Scheduling"},
-                {title: "Ministry Safe", category: "Enterprise"},
-                {title: "SwingAway", category: "eCommerce"}
-            ]
-        };
-
-        this.handleFilter = this.handleFilter.bind(this);
-    }
-    // Functions you can use if you do a class bassed component:
+      // Functions you can use if you do a class bassed component:
     // State
     // Lifecycle Hooks 
-
-    handleFilter(filter) {
+  handleFilter(filter) {
+    this.setState({
+      data: this.state.data.filter(item => {
+        return item.category === filter;
+      })
+    });
+  }
+  getPortfolioItems() {
+    axios
+      .get("https://hannahdonaldson2.devcamp.space/portfolio/portfolio_items")
+      .then(response => {
         this.setState({
-            data: this.state.data.filter(item => {
-                return item.category === filter;
-            })
-        })
-    }
-
-    portfolioItems() {
-        return this.state.data.map(item => {
-            return <PortfolioItem title={item.title} url={"google.com"} />;
+          data: response.data.portfolio_items
         });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+  portfolioItems() {
+    return this.state.data.map(item => {
+      return (
+        <PortfolioItem
+          key={item.id}
+          item = {item}
+        />
+      );
+    });
+  }
+  componentDidMount() {
+    this.getPortfolioItems();
+  }
+  render() {
+    if (this.state.isLoading) {
+      return <div>Loading...</div>;
     }
-    
-    render() {
-        if (this.state.isLoading) {
-            return <div>Loading...</div>
-        } //else 
-
-        return (
-            <div>
-                <h2>{this.state.pageTitle}</h2>
-
-                <button onClick ={() => this.handleFilter('eCommerce')}>eCommerce</button>
-                <button onClick ={() => this.handleFilter('Scheduling')}>Scheduling</button>
-                <button onClick ={() => this.handleFilter('Enterprise')}>Enterprise</button>
-
-                {this.portfolioItems()}
-            </div>
-        )
-    }
+    return (
+      <div>
+        <h2>{this.state.pageTitle}</h2>
+        <button onClick={() => this.handleFilter("eCommerce")}>
+          eCommerce
+        </button>
+        <button onClick={() => this.handleFilter("Scheduling")}>
+          Scheduling
+        </button>
+        <button onClick={() => this.handleFilter("Enterprise")}>
+          Enterprise
+        </button>
+        {this.portfolioItems()}
+      </div>
+    );
+  }
 }
